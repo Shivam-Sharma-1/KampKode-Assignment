cd1e4e6a075ff3f168ceb57916796d77d56cb21f
Shivam77607 <shivamsharma77607@gmail.com>
2024-08-27 19:04:46 +0530

    refactor: Improve responsiveness of HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,30 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OrbitingCircles from "./magicui/orbiting-circles";
 import { IoMdArrowForward } from "react-icons/io";
 import Image from "next/image";
 
 function HeroSection() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event: {
+      matches: boolean | ((prevState: boolean) => boolean);
+    }) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <section>
       <div className="relative flex h-full py-20 md:p-0 min-h-dvh w-full flex-col items-center justify-center overflow-hidden bg-background">
@@ -23,7 +42,7 @@ function HeroSection() {
             </p>
             <p> Join 4,000+ companies already growing</p>
           </div>
-          <div className="flex flex-col xs:flex-row items-center gap-2 xs:gap-6 mt-4">
+          <div className="flex flex-col-reverse xs:flex-row items-center gap-2 xs:gap-6 mt-4">
             <button className="h-12 items-center justify-center rounded-[8px] border border-slate-200 bg-white px-6 font-medium text-slate-700 cursor-pointer z-[10] flex gap-2 xs:w-fit w-full">
               <IoMdArrowForward className="w-5 h-5" />
               Start Learning Today
@@ -39,19 +58,19 @@ function HeroSection() {
           className="border-none bg-transparent"
           duration={20}
           delay={20}
-          radius={220}
+          radius={isMobile ? 100 : 220}
         ></OrbitingCircles>
 
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
           delay={20}
-          radius={300}
+          radius={isMobile ? 160 : 300}
         >
           <Image
             src={"/icons/css.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -60,12 +79,12 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={300}
+          radius={isMobile ? 160 : 300}
         >
           <Image
             src={"/icons/gitlab.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -74,14 +93,14 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={380}
+          radius={isMobile ? 220 : 380}
           delay={2}
           reverse
         >
           <Image
             src={"/icons/react.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -90,14 +109,14 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={380}
+          radius={isMobile ? 220 : 380}
           delay={12}
           reverse
         >
           <Image
             src={"/icons/aws.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -106,13 +125,13 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={460}
+          radius={isMobile ? 280 : 460}
           delay={4}
         >
           <Image
             src={"/icons/nodejs.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -121,13 +140,13 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={460}
+          radius={isMobile ? 280 : 460}
           delay={14}
         >
           <Image
             src={"/icons/flutter.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -136,14 +155,14 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={540}
+          radius={isMobile ? 340 : 540}
           delay={6}
           reverse
         >
           <Image
             src={"/icons/html5.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -152,14 +171,14 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={540}
+          radius={isMobile ? 340 : 540}
           delay={1}
           reverse
         >
           <Image
             src={"/icons/android.svg"}
-            width={60}
-            height={60}
+            width={isMobile ? 40 : 60}
+            height={isMobile ? 40 : 60}
             alt="logo"
             className="opacity-50"
           />
@@ -168,7 +187,7 @@ function HeroSection() {
         <OrbitingCircles
           className="border-none bg-transparent"
           duration={20}
-          radius={620}
+          radius={isMobile ? 420 : 620}
         ></OrbitingCircles>
       </div>
     </section>
