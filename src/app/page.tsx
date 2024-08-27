896c7a5b4e2d5c2e80cbaaaea58afe57742bfdf2
Shivam77607 <shivamsharma77607@gmail.com>
2024-08-27 17:17:48 +0530

    feat: Update page layout with HeroSection and Navbar components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,13 @@
 "use-client";
 
-import { FloatingNav } from "@/components/ui/floating-navbar";
-import Image from "next/image";
-import { IoIosArrowDown } from "react-icons/io";
+import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="px-12">
-      <FloatingNav
-        navItems={[
-          {
-            name: "Home",
-            link: "/",
-          },
-          {
-            name: "Products",
-            link: "/",
-            icon: <IoIosArrowDown className="h-5 w-5" />,
-          },
-          {
-            name: "Resources",
-            link: "/",
-            icon: <IoIosArrowDown className="h-5 w-5" />,
-          },
-          { name: "Pricing", link: "/" },
-        ]}
-        className="w-full"
-      />
+      <Navbar />
+      <HeroSection />
       <div className="grid grid-cols-1 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md">
         <p className="dark:text-white text-neutral-600 text-center text-4xl mt-40 font-bold">
           Scroll back up to reveal Navbar
